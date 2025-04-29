982e7720e823ca2d96fd753de14c46cb1758ad58
Askar-A-A <askar.a.a.dev@gmail.com>
2025-04-29 16:21:34 +0500

    the 400px layout buggs has been fixed

--- theater_config/urls.py
+++ theater_config/urls.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theater_cms.urls')),  # Include your app's URLs at root level
+]
+
+# Translated URLs
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
     path('', include('cms.urls')),  # Keep the CMS URLs last
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
