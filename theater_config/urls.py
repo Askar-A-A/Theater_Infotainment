4d5abfc99d4ca8e0f1a34c60a59a231ce9134f59
Askar-A-A <askar.a.a.dev@gmail.com>
2025-06-21 23:27:15 +0500

    further fixing routing

--- theater_config/urls.py
+++ theater_config/urls.py
@@ -7,13 +7,14 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theater_cms.urls')),  # Include your app's URLs at root level
+    path('', include('cms.urls')),  # CMS URLs - moved outside i18n_patterns
 ]
 
-# Translated URLs
-urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('', include('cms.urls')),  # Keep the CMS URLs last
-)
+# Remove the i18n_patterns section since you're not using Django's i18n system
+# urlpatterns += i18n_patterns(
+#     path('admin/', admin.site.urls),
+#     path('', include('cms.urls')),  # Keep the CMS URLs last
+# )
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
